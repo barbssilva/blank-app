9a74971ca09d404a69f84947250619019f60d975
barbssilva <barbarasilva@anglotex.pt>
2025-09-18 15:13:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,10 +61,8 @@ if cliente:
             excel_output2 = os.path.join(temp_dir, base_name + "_new.xlsx")
             excel_final = os.path.join(temp_dir, base_name + "_processed.xlsx")
 
-            
-            with st.spinner("⏳ Por favor, aguarde..."):
-                # aqui corre o teu processo longo
-                time.sleep(5)  # exemplo
+            placeholder = st.empty()
+            placeholder.info("⏳ Por favor aguarde...")
                 
             # CRIAR UM FICHEIRO APENAS COM AS PALAVRAS SELECIONADAS
             keywords1= ['design front sheet','design spec','proto','sms','gold spec','grading']
@@ -90,6 +88,7 @@ if cliente:
             #juntar os dois ficheiros 
             concat(excel_output1, excel_output2, excel_final)
 
+            placeholder.empty()
             st.success("Processo terminado!")
 
             # Abrir o ficheiro Excel processado para download
